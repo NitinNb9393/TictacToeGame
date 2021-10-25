1f1ed93390dffbf67f2a018be724e335d8632870
NitinNb9393 <nitinbabar9393@gmail.com>
2021-10-25 19:46:56 +0530

    Added: Choosing X or O

--- src/com/bridgelab/tictactocgame/TicTacToeGam.java
+++ src/com/bridgelab/tictactocgame/TicTacToeGam.java
@@ -1,11 +1,15 @@
 package com.bridgelab.tictactocgame;
+import java.util.Scanner;
 
 public class TicTacToeGam {
 	public static char[] element;
+	static char userMark, computerMark;
+	static Scanner scan = new Scanner(System.in);
 
 	public static void main(String args[]) {
 		System.out.println("Welcome to the Tic Tac Toe Game ");
 		boardCreation();
+		choosingXorO();
 	}
 
 	public static void boardCreation() {
@@ -16,4 +20,21 @@ public class TicTacToeGam {
 		System.out.println("Empty board created");
 	}
 
+	public static void choosingXorO() {
+		System.out.println("Choose 1 for 'X' or Choose 2 for 'O' as your mark");
+		int option = scan.nextInt();
+		switch (option) {
+		case 1:
+			userMark = 'X';
+			computerMark = 'O';
+			break;
+		case 2:
+			userMark = 'O';
+			computerMark = 'X';
+			break;
+		default:
+			System.out.println("Your input is invalid");
+			choosingXorO();
+		}
+	}
 }
